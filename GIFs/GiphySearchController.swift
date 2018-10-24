ff6447b587a8ab5b9a6361cc9a69f3a6be01600a
Matt Rubin <opensource@mattrubin.me>
2018-10-24 11:04:02 -0400

    Dispatch the whole API completion block to the main thread

--- GIFs/GiphySearchController.swift
+++ GIFs/GiphySearchController.swift
@@ -66,28 +66,31 @@ class GiphySearchController {
         // Make a new API request, and retain the search query and Operation.
         print("Searching for \"\(query)\"...")
         let searchOperation = GiphyCore.shared.search(query, limit: pageSize) { [weak self] (response, error) in
-            // Recover a strong reference to self, or abort.
-            guard let self = self else {
-                return
-            }
-
-            // Before processing any search results, guard against race conditions and operation cancellation mistakes
-            // by ensuring that this completion block is being called for the current search-in-progress. This can be
-            // confimed by checking that the current search-in-progress has the expected query string, and by checking
-            // that the search operation is in a finished state.
-            guard let searchInProgress = self.searchInProgress,
-                searchInProgress.query == query,
-                searchInProgress.operation.isFinished else {
-                    print("Completion block called does not match searchInProgress:")
-                    print("     Completion block query: \(query)")
-                    print("     searchInProgress query: \(String(describing: self.searchInProgress?.query))")
+            // Dispatch back to the main queue before making any changes.
+            DispatchQueue.main.async {
+                // Recover a strong reference to self, or abort.
+                guard let self = self else {
                     return
-            }
+                }
 
-            // Discard the completed search operation and handle the search results.
-            print("Search operation completed.")
-            self.searchInProgress = nil
-            self.handleCompletedSearch(query: query, response: response, error: error)
+                // Before processing any search results, guard against race conditions and operation cancellation
+                // mistakes by ensuring that this completion block is being called for the current search-in-progress.
+                // This can be confimed by checking that the current search-in-progress has the expected query string,
+                // and by checking that the search operation is in a finished state.
+                guard let searchInProgress = self.searchInProgress,
+                    searchInProgress.query == query,
+                    searchInProgress.operation.isFinished else {
+                        print("Completion block called does not match searchInProgress:")
+                        print("     Completion block query: \(query)")
+                        print("     searchInProgress query: \(String(describing: self.searchInProgress?.query))")
+                        return
+                }
+
+                // Discard the completed search operation and handle the search results.
+                print("Search operation completed.")
+                self.searchInProgress = nil
+                self.handleCompletedSearch(query: query, response: response, error: error)
+            }
         }
         searchInProgress = (query: query, operation: searchOperation)
     }
@@ -119,24 +122,18 @@ class GiphySearchController {
             // Unfortunately, when an error occurs, the GiphyCoreSDK returns only an Error object with strings that are
             // not tailored for display to the user, and it does not return a GPHMeta object from which we can extract
             // failure status codes. Without a useful specific error message, show a generic one.
-            DispatchQueue.main.async {
-                print("ERROR: \(error)")
-                self.delegate?.update(withErrorMessage: "🤕 Something went wrong.")
-            }
+            print("ERROR: \(error)")
+            self.delegate?.update(withErrorMessage: "🤕 Something went wrong.")
         } else if let response = response, let data = response.data, let pagination = response.pagination {
             // TODO: Check reponse metadata?
             // TODO: Implement reponse pagination?
-            DispatchQueue.main.async {
-                let searchResults = SearchResults(query: query, media: data)
-                self.latestSearchResults = searchResults
-                self.delegate?.update(with: searchResults)
-            }
+            let searchResults = SearchResults(query: query, media: data)
+            self.latestSearchResults = searchResults
+            self.delegate?.update(with: searchResults)
         } else {
             // If the API call didn't return an error, and also didn't return a response object with useable data, then
             // show a generic error message.
-            DispatchQueue.main.async {
-                self.delegate?.update(withErrorMessage: "🤔 Something went wrong.")
-            }
+            self.delegate?.update(withErrorMessage: "🤔 Something went wrong.")
         }
     }
 }
